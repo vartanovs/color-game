cefb3d71ae9a490b63f4a93ea791a100dfaf910b
Serge Vartanov <vartanov.s@gmail.com>
2018-08-30 23:09:02 -0700

    Color Game HTML, CSS and JS files complete

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ A responsive web application game built to test a user's ability to guess a colo
 * You earn a point for each card remaining on the screen.
 * Play up to ten rounds to get the highest score you can.
 * Reset the game by selecting **start over** or changing difficulty.
-* Try to get a high score of 40 on easy, 90 on medium and 160 on hard mode!
+* Try to get a high score of 30 on easy, 60 on medium and 90 on hard mode!
 
 ## RGB Color Model
 This is an *additive color model* named after the initials of the three primary colors: **Red**, **Green** and **Blue**. Red, Green and Blue right are added together to produce a color. The intensity of each primary color is indicated by a number, from 0 (no color) to 255 (highest intensity).
@@ -25,7 +25,7 @@ For example, a value of (255, 0, 0) would create vivid red, while (0, 255, 0) wo
 This project is an expansion of the "Color Game" project that is part of the [Colt Steele](https://github.com/Colt) ["Web Developer Bootcamp"](https://www.udemy.com/the-web-developer-bootcamp/) on [Udemy](https://www.udemy.com/).
 Feature expansions beyond the standard project include:
 * Change to number of cards and responsive layout in "Easy" and "Hard" difficulty.
-* Addition of "Medium" difficulty setting with 9-cards.
+* Addition of "Medium" difficulty setting with 6-cards.
 * Addition of multiple (up to 10) rounds and game reset.
 * Ability to keep track of score for a single level.
 * Ability to keep track of score across ten rounds.
---
+++ color-game.js
@@ -0,0 +1,170 @@
+// Declare method container for colorGame selectors and variables
+const colorGame = {
+  colors: [],
+  difficultyButtons: document.getElementsByClassName('difficulty'),
+  goalColor: '',
+  h1: document.getElementsByTagName('h1')[0],
+  messageDisplay: document.getElementById('message'),
+  resetButton: document.getElementById('reset'),
+  points: 6,
+  pointsSpan: document.getElementById('points'),
+  round: 1,
+  roundSpan: document.getElementById('round'),
+  squareCount: 6,
+  squares: document.getElementsByClassName('square'),
+  targetColor: document.getElementById('targetColor'),
+  total: 0,
+  totalSpan: document.getElementById('total'),
+  win: false,
+};
+
+// Helper function to generate an array of random colors
+const generateRandomColors = function generateRandomColors() {
+  // Inner helper functions generate three random numbers, 0 - 255
+  const randomNumber = function randomNumber() {
+    return Math.floor(Math.random() * 256);
+  };
+  const randomColor = function randomColor() {
+    return `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`;
+  };
+  // Create and return a colorArray filled with a random color for every active square
+  const colorArray = [];
+  for (let i = 0; i < colorGame.squareCount; i += 1) {
+    colorArray.push(randomColor());
+  }
+  return colorArray;
+};
+
+// Helper function to turn all squares and header to winning color
+const changeColors = function changeColors(winningColor) {
+  // Loop through all squares, change to winning color
+  for (let i = 0; i < colorGame.squareCount; i += 1) {
+    // Change each color to match winning color
+    colorGame.squares[i].style.backgroundColor = winningColor;
+  }
+  colorGame.h1.style.backgroundColor = winningColor;
+};
+
+// Helper function to randomly select winner from colors array
+const pickWinner = function pickWinner() {
+  const index = Math.floor(Math.random() * colorGame.squareCount);
+  return colorGame.colors[index];
+};
+
+// Helper function to reset the header and settings bar
+const resetDisplay = function resetDisplay() {
+  colorGame.h1.style.backgroundColor = 'steelblue';
+  colorGame.targetColor.textContent = colorGame.goalColor;
+  colorGame.resetButton.textContent = 'Reset';
+  colorGame.messageDisplay.textContent = '';
+};
+
+// Helper function to color in squares according to random color array
+const fillSquares = function fillSquares() {
+  for (let i = 0; i < colorGame.squares.length; i += 1) {
+    if (i < colorGame.squareCount) {
+      // If square is within squareCount, make square visibile and add color
+      colorGame.squares[i].style.display = 'block';
+      colorGame.squares[i].style.backgroundColor = colorGame.colors[i];
+    } else {
+      // If square is beyond squareCount, make square invisible
+      colorGame.squares[i].style.display = 'none';
+    }
+  }
+};
+
+// Helper function to reset score if user hasn't won, update total if user earns a win
+const resetPoints = function resetScore(win) {
+  if (win) {
+    // If user has won, add the round points to their total score
+    colorGame.round += 1;
+    colorGame.total = colorGame.points + colorGame.total;
+    colorGame.points = colorGame.squareCount;
+  } else {
+    // If user is resetting without a win, reset to 1st round defaults
+    colorGame.round = 1;
+    colorGame.total = 0;
+    colorGame.points = colorGame.squareCount;
+  }
+};
+
+// Helper function to update score bar
+const updateScore = function updateScore() {
+  colorGame.roundSpan.textContent = `Round: ${colorGame.round}`;
+  colorGame.pointsSpan.textContent = `Points: ${colorGame.points}`;
+  colorGame.totalSpan.textContent = `Total Score: ${colorGame.total}`;
+};
+
+// Helper function to initialize game:
+const resetGame = function resetGame() {
+  // Generate new array of random colors
+  colorGame.colors = generateRandomColors(colorGame.squareCount);
+  // Randomly select a new winning color
+  colorGame.goalColor = pickWinner();
+  // Set display with winning color
+  resetDisplay();
+  // Set colors of squares, hide unnecessary squares
+  fillSquares(colorGame.squareCount);
+  // Update score variables and score bar
+  resetPoints(colorGame.win);
+  updateScore();
+  // Set colorGame.win back to false;
+  colorGame.win = false;
+};
+
+// Event Listener for Reset Button
+colorGame.resetButton.addEventListener('click', resetGame);
+
+// Event Listeners for Squares with Correct and Incorrect Guess Logic
+for (let i = 0; i < colorGame.squares.length; i += 1) {
+  colorGame.squares[i].addEventListener('click', () => {
+    // Grab color of user selected square
+    let selectedColor = colorGame.squares[i].style.backgroundColor;
+    // Compare user selected color to goal color
+    if (selectedColor === colorGame.goalColor) {
+      // Correct user case logic
+      colorGame.messageDisplay.textContent = 'Correct!';
+      colorGame.resetButton.textContent = 'Next Round';
+      changeColors(selectedColor);
+      colorGame.win = true;
+    } else if (colorGame.squares[i].style.display === 'block') {
+      // Incorrect user case logic
+      colorGame.squares[i].style.background = '#232323';
+      colorGame.messageDisplay.textContent = 'Try Again';
+      colorGame.points -= 1;
+      colorGame.pointsSpan.textContent = `Points: ${colorGame.points}`;
+    }
+  });
+}
+
+// Event Listeners for Difficulty Selection and Difficulty Reset
+for (let i = 0; i < colorGame.difficultyButtons.length; i += 1) {
+  colorGame.difficultyButtons[i].addEventListener('click', () => {
+    // Deselect all difficulty buttons
+    for (let j = 0; j < colorGame.difficultyButtons.length; j += 1) {
+      colorGame.difficultyButtons[j].classList.remove('selected');
+    }
+    // Reselect only the selected difficulty
+    colorGame.difficultyButtons[i].classList.add('selected');
+    // Set squareCount to 3 for Easy Mode, 6 for Medium Mode, 9 for Hard Mode
+    switch (colorGame.difficultyButtons[i].textContent) {
+      case 'Easy':
+        colorGame.squareCount = 3;
+        break;
+      case 'Medium':
+        colorGame.squareCount = 6;
+        break;
+      case 'Hard':
+        colorGame.squareCount = 9;
+        break;
+      default:
+        break;
+    }
+    // Reset game with updated squareCount
+    colorGame.win = false;
+    resetGame();
+  });
+}
+
+// Initialize Game:
+resetGame();
